9dfcfa11b8de6b25c375474e0eb39a07e1dd3cb2
VincentChen6345 <vincent.chen6345@hotmail.com>
2023-03-12 15:08:29 +1100

    editing task item

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const taskList = document.getElementById("taskList-container");
 const addTaskBTN = document.getElementById("addtask-btn");
-
+const taskText = document.getElementById("task-item_text");
 //when add task btn is clicked
 //create new list element, extract string from input field, extract priority from select dropdown , place list element in an array
+
+const check = () => {
+  taskText.classList.toggle("strikethrough");
+};

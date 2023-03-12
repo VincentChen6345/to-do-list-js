6d1e9abbca7e09f5eddf57a314d2168383819388
VincentChen6345 <vincent.chen6345@hotmail.com>
2023-03-12 12:48:05 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const taskList = document.getElementById("taskList-container");
+const addTaskBTN = document.getElementById("addtask-btn");
+
+//when add task btn is clicked
+//create new list element, extract string from input field, extract priority from select dropdown , place list element in an array
